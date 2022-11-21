4edec7364699b285cedc492eb7ae047016c1d6ac
midwestE <midwestE@devbunker.com>
2022-11-21 07:06:21 -0600

    Account for empty tag return

--- src/tag.php
+++ src/tag.php
@@ -2,9 +2,11 @@
 
 namespace _woo;
 
-use WC_Product;
-
-function product_tag_id_from_slug( string $slug ) {
-	$term = \get_term_by( 'slug', $slug, 'product_tag' );
-	return $term->term_id;
+function product_tag_id_from_slug(string $slug)
+{
+    $term = \get_term_by('slug', $slug, 'product_tag');
+    if (!$term instanceof \WP_Term || !is_array($term) || empty($term->term_id)) {
+        return;
+    }
+    return $term->term_id;
 }
