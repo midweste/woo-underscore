8e48d2d6607f6b9914d394c8592a854e0c55b4a2
midwestE <midwestE@devbunker.com>
2022-12-16 16:09:22 -0600

    Fix check for tag id by slug

--- src/tag.php
+++ src/tag.php
@@ -5,8 +5,8 @@ namespace _woo;
 function product_tag_id_from_slug(string $slug)
 {
     $term = \get_term_by('slug', $slug, 'product_tag');
-    if (!$term instanceof \WP_Term || !is_array($term) || empty($term->term_id)) {
+    if (!$term instanceof \WP_Term && !is_array($term)) {
         return;
     }
-    return $term->term_id;
+    return (is_array($term)) ? $term['term_id'] : $term->term_id;
 }
