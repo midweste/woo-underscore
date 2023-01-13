2692aaa7ef8be84eb7be5e2ae9978c81b4878d04
midwestE <midwestE@devbunker.com>
2023-01-13 13:04:45 -0600

    Add check is page tab section

--- src/conditional.php
+++ src/conditional.php
@@ -2,15 +2,25 @@
 
 namespace _woo;
 
-function is_order_admin_page() {
-	global $pagenow;
-	if (
-		empty( $pagenow )
-		|| $pagenow !== 'edit.php'
-		|| empty( $_GET['post_type'] )
-		|| $_GET['post_type'] !== 'shop_order'
-	) {
-		return false;
-	}
-	return true;
+function is_order_admin_page()
+{
+    global $pagenow;
+    if (
+        empty($pagenow)
+        || $pagenow !== 'edit.php'
+        || empty($_GET['post_type'])
+        || $_GET['post_type'] !== 'shop_order'
+    ) {
+        return false;
+    }
+    return true;
+}
+
+function is_page_tab_section(string $page, string $tab, string $section): bool
+{
+    $get = $_GET;
+    $is_page = (isset($get['page']) && $get['page'] === $page) ? true : false;
+    $is_tab = (isset($get['tab']) && $get['tab'] === $tab) ? true : false;
+    $is_section = (isset($get['section']) && $get['section'] === $section) ? true : false;
+    return ($is_page && $is_tab && $is_section) ? true : false;
 }
